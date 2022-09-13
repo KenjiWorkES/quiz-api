5d0e13492df99e76a0fb7b49057ed794add0eb7c
KenjiWorkES <lucas.kenjies@gmail.com>
2022-09-13 15:34:23 -0300

    add pages folder

--- quiz-api/src/App.tsx
+++ quiz-api/src/App.tsx
@@ -1,12 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { defaultTheme } from '@/themes';
 import { Box } from '@chakra-ui/react';
+import { Dashboard } from './components/pages';
 
 const App: React.FC = () => {
   return (
     <ChakraProvider theme={defaultTheme}>
       <Box bg="brand.black" color="brand.white">
-        Test
+        <Dashboard />
       </Box>
     </ChakraProvider>
   );
