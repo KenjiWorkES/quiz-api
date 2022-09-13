9d318880ca74f6875f64f56e5df1d8a00a58b928
John Mello <johncharlesmello@gmail.com>
2022-09-13 19:27:58 -0300

    call Singup

--- quiz-api/src/App.tsx
+++ quiz-api/src/App.tsx
@@ -1,13 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { defaultTheme } from '@/themes';
-import { Box } from '@chakra-ui/react';
-import { Dashboard } from './components/pages';
+import { ChakraProvider } from "@chakra-ui/react";
+import { defaultTheme } from "@/themes";
+import { Box } from "@chakra-ui/react";
+import { Dashboard } from "./components/pages";
+import Login from "./components/pages/Login/test";
+import Signup from "./components/pages/Signup";
 
 const App: React.FC = () => {
   return (
     <ChakraProvider theme={defaultTheme}>
       <Box bg="brand.black" color="brand.white">
-        <Dashboard />
+        <Signup />
       </Box>
     </ChakraProvider>
   );
