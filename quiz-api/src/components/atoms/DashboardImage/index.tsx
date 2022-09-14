69aaa93831b64e1cfbf538a3bb1fc65c5e05f658
KenjiLucas <802.182@alunos.unigran.br>
2022-09-14 08:40:35 -0400

    add Image component to DashboardImage

--- quiz-api/src/components/atoms/DashboardImage/index.tsx
+++ quiz-api/src/components/atoms/DashboardImage/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Image } from '@chakra-ui/react';
 
 const DashboardImage = () => {
-  return <div>DashboardImage</div>;
+  return (
+    <Image
+      alt="Perfil Image"
+      src="https://img.freepik.com/free-photo/pleasant-looking-serious-man-stands-profile-has-confident-expression-wears-casual-white-t-shirt_273609-16959.jpg?w=2000"
+    />
+  );
 };
 
 export default DashboardImage;
---
+++ quiz-api/src/components/atoms/index.ts
@@ -0,0 +1,3 @@
+export { default as DashboardForm } from './DashboardForm';
+export { default as DashboardImage } from './DashboardImage';
+export { default as DashboardTitle } from './DashboardTitle';
