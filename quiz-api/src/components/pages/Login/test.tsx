f1df8303d558fe7e4964a476a83f8157bd360f0a
John Mello <johncharlesmello@gmail.com>
2022-09-13 23:04:16 -0300

    redo login layout

--- quiz-api/src/components/pages/Login/test.tsx
+++ quiz-api/src/components/pages/Login/test.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Center,
   Flex,
@@ -10,24 +10,47 @@ import {
   Heading,
   HStack,
   VStack,
-} from "@chakra-ui/react";
+  FormControl,
+  FormLabel,
+  Link,
+} from '@chakra-ui/react';
 
 const Login: React.FC = () => {
   return (
-    <Center w="100vw" h="100vh">
-      <VStack>
-        <Heading>Login</Heading>
-        <Text>Email</Text>
-        <Input maxW="85%" />
-        <Spacer />
-        <Text>Password</Text>
-        <Input maxW="85%" type="password" />
-        <Spacer />
-        <Button color="blackAlpha.900" bg="gray.300">
-          Sign up
-        </Button>
-        <Spacer />
-        <Text>Forgot your credentials?</Text>
+    <Center
+      w="100vw"
+      h="100vh"
+      backgroundImage="url(/assets/img/login-bg.jpeg)"
+      backgroundSize="cover"
+      backgroundPosition="top"
+    >
+      <VStack
+        bg="white"
+        color="black"
+        minH="70%"
+        spacing={12}
+        borderRadius="2xl"
+        boxShadow="md"
+      >
+        <Heading mt={6}>Login</Heading>
+
+        <FormControl>
+          <Flex direction="column" rowGap={6} p={4}>
+            <Box>
+              <FormLabel>Email</FormLabel>
+              <Input />
+            </Box>
+            <Box>
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </Box>
+            <Button color="blackAlpha.900" bg="gray.300" fontSize="smaller">
+              Submit
+            </Button>
+          </Flex>
+        </FormControl>
+
+        <Link fontSize={12}>Forgot your credentials?</Link>
       </VStack>
     </Center>
   );
