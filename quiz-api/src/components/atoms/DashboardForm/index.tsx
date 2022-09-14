3c2255d9962ab3b3186e301d179adbae8a047558
KenjiLucas <802.182@alunos.unigran.br>
2022-09-14 09:23:58 -0400

    add DashboardForm components

--- quiz-api/src/components/atoms/DashboardForm/index.tsx
+++ quiz-api/src/components/atoms/DashboardForm/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
 
-const DashboardForm = () => {
-  return <div>DashboardForm</div>;
+const DashboardForm: React.FC = () => {
+  return (
+    <Box>
+      <FormControl>
+        <FormLabel>Old Password</FormLabel>
+        <Input type="password" />
+      </FormControl>
+      <FormControl>
+        <FormLabel>New Password</FormLabel>
+        <Input type="password" />
+      </FormControl>
+      <Button colorScheme="purple"></Button>
+    </Box>
+  );
 };
 
 export default DashboardForm;
