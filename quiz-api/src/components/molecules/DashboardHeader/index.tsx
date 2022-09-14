0893bc356ca5f0ba58029ff9b0e288db7ac9bd61
KenjiLucas <802.182@alunos.unigran.br>
2022-09-14 08:45:45 -0400

    add components to DashboardHeader

--- quiz-api/src/components/molecules/DashboardHeader/index.tsx
+++ quiz-api/src/components/molecules/DashboardHeader/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { DashboardImage, DashboardTitle } from '@/components/atoms';
+import { Box } from '@chakra-ui/react';
 
 const DashboardHeader = () => {
-  return <div>DashboardHeader</div>;
+  return (
+    <Box display="flex" flexDirection="column">
+      <Box></Box>
+      <Box display="flex">
+        <DashboardImage />
+        <DashboardTitle />
+      </Box>
+    </Box>
+  );
 };
 
 export default DashboardHeader;
